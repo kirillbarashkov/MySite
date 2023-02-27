bc42c6001463a688f7f3e8b4b9e9233543a937eb
kirillbarashkov <masterttu@gmail.com>
2023-02-27 22:50:45 +0300

    Site content updated

--- MySite/Main/views.py
+++ MySite/Main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 from .models import *
 
@@ -11,10 +11,13 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def main_page(request):
     posts = Main.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'Main/main_page.html', context=context)
 
@@ -39,5 +42,27 @@ def login(request):
     return HttpResponse('Авторизация')
 
 
+def pageNotFound(request, exseption):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
 def show_post(request, post_id):
     return HttpResponse(f'Отображение проекта с id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Main.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по категориям',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'Main/main_page.html', context=context)
